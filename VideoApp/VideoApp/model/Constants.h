0fa1096507acf0820095921b697b19cf85e84933
An Cheng <acu11988@gmail.com>
2017-11-19 04:53:03 +0800

    rename mold test

--- VideoApp/VideoApp/model/Constants.h
+++ VideoApp/VideoApp/model/Constants.h
@@ -21,7 +21,7 @@
 
 #ifdef rep1
 
-#define VIDEO_TITLES   @[@"Welcome" , @"Tools" ,@"Area of interest" , @"moving around inspection" ,@"Affected items",@"Home Owners Stuff" ,@"mold" ,@"Lift Test" ,@"Mechanicals" ,@"Electric" ,@"Dehumidifier" ,@"Wrapping it up" ,@"Exterior inspection"]
+#define VIDEO_TITLES   @[@"Welcome" , @"Tools" ,@"Area of interest" , @"moving around inspection" ,@"Affected items",@"Home Owners Stuff" ,@"Mold Test" ,@"Lift Test" ,@"Mechanicals" ,@"Electric" ,@"Dehumidifier" ,@"Wrapping it up" ,@"Exterior inspection"]
 
 #endif
 
